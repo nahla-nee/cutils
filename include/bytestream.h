a4e09f9507805ba23b09ece0e51cf78d71594d40
zee220 <mustafa@zee.technology>
2019-08-08 12:41:03 -0600

    added bytestreams

---
+++ include/bytestream.h
@@ -0,0 +1,54 @@
+#ifndef CUTILS_BYTESTREAM_H
+#define CUTILS_BYTESTREAM_H
+
+#include "errors.h"
+
+#include <stddef.h>
+#include <string.h>
+
+//really similar to string, except it has no null temrinator
+
+typedef struct cutilsByteStream{
+	unsigned char *stream;
+	size_t size;
+	size_t capacity;
+} cutilsByteStream;
+
+int cutilsByteStreamInit(cutilsByteStream *stream, size_t capacity);
+int cutilsByteStreamCopy(cutilsByteStream *dst, cutilsByteStream *src);
+void cutilsByteStreamMove(cutilsByteStream *dst, cutilsByteStream *src);
+void cutilsByteStreamSwap(cutilsByteStream *a, cutilsByteStream *b);
+void cutilsByteStreamFree(cutilsByteStream *stream);
+
+int cutilsByteStreamResize(cutilsByteStream *stream, size_t size);
+int cutilsByteStreamReserve(cutilsByteStream *stream, size_t capacity);
+
+int cutilsByteStreamAppendByte(cutilsByteStream *stream, unsigned char x);
+int cutilsByteStreamAppendStream(cutilsByteStream *stream, cutilsByteStream *x);
+
+int cutilsByteStreamInsertByte(cutilsByteStream *stream, unsigned char x, size_t index);
+int cutilsByteStreamInsertStream(cutilsByteStream *stream, cutilsByteStream *x, size_t index);
+
+#define cutilsByteStreamAppend(STREAM, X) _Generic((X),\
+		char: cutilsByteStreamAppendByte,\
+		const char: cutilsByteStreamAppendByte,\
+		unsigned char: cutilsByteStreamAppendByte,\
+		const unsigned char: cutilsByteStreamAppendByte,\
+		int: cutilsByteStreamAppendByte,\
+		const int: cutilsByteStreamAppendByte,\
+		cutilsByteStream*: cutilsByteStreamAppendStream,\
+		const cutilsByteStream*: cutilsByteStreamAppendStream\
+	) (STREAM, X)
+
+#define cutilsByteStreamInsert(STREAM, X, INDEX) _Generic((X),\
+		char: cutilsByteStreamInsertByte,\
+		const char: cutilsByteStreamInsertByte,\
+		unsigned char: cutilsByteStreamInsertByte,\
+		const unsigned char: cutilsByteStreamInsertByte,\
+		int: cutilsByteStreamInsertByte,\
+		const int: cutilsByteStreamInsertByte,\
+		cutilsByteStream*: cutilsByteStreamInsertStream,\
+		const cutilsByteStream*: cutilsByteStreamInsertStream\
+	) (STREAM, X, INDEX)
+
+#endif
